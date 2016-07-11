c251b9d67a9d63d33ea6c860ef55839240f712e3
GeneralMcpaws <dimitris.tzivakis@gmail.com>
2016-07-11 13:03:18 +0300

    Replaced MusicAudio script with MusicManager and made PersistentMusic for all scenes

--- Fox_Goose 3rd Edition/Assets/Scripts/LoadOnClick.cs
+++ Fox_Goose 3rd Edition/Assets/Scripts/LoadOnClick.cs
@@ -6,6 +6,7 @@ public class LoadOnClick : MonoBehaviour {
 	public void LoadScene(int level)
     {   
         Application.LoadLevel(level);
+        
     }
 
     public void Quit()
---
+++ Fox_Goose 3rd Edition/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour
+{
+    //public AudioClip[] levelMusicArray;
+
+    static MusicManager instance = null;
+
+    private AudioSource audioSource;
+   
+  
+
+    void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    
+}
